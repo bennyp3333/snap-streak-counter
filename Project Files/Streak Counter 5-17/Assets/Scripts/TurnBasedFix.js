a3de5441c20f2a165c836145f4ef7ced729cbf1b
benjamin-subtropic <bennyp3333@gmail.com>
2025-12-20 18:58:02 -0600

    Turn based capture crash fix

---
+++ Project Files/Streak Counter 5-17/Assets/Scripts/TurnBasedFix.js
@@ -0,0 +1,36 @@
+// @input Component.ScriptComponent turnBased {"label": "Turn Based Component"}
+
+script.turnBased.endTurn = async function(){
+    if (this.requireTurnSubmission) {
+        //this.turnDataController.setCaptureSnap(!!this.autoCaptureHelper);
+        this.turnDataController.endTurn();
+        const isFinalTurn = await this.turnDataController.isFinalTurn();
+        if (isFinalTurn) {
+            this.sceneObjectsController.onGameOver();
+            this.onGameOver.trigger();
+        } else {
+            this.onTurnEnd.trigger();
+        }
+    }
+}
+/*
+function onTapped(eventData)
+{
+    script.turnBased.endTurn();
+}
+var event = script.createEvent("TapEvent");
+event.bind(onTapped);
+
+// Automatically end turn when user captures a snap
+script.createEvent('SnapRecordStopEvent').bind(function() {
+    print('SnapRecordStopEvent - ending turn');
+    script.turnBased.setCurrentTurnVariable('sendTimestamp', Date.now());
+    script.turnBased.endTurn();
+});
+
+script.createEvent('SnapImageCaptureEvent').bind(function() {
+    print('SnapImageCaptureEvent - ending turn');
+    script.turnBased.setCurrentTurnVariable('sendTimestamp',Date.now());
+    script.turnBased.endTurn();
+});
+*/
